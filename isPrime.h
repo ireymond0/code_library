31e24d79741691720eb9c50e022a0b2854641f11
Ian Reymond <ian@luxion.com>
2015-11-14 14:05:56 -0800

    added isPrime.h, the utiltity returns bool value that tells you if integer is prime or not

---
+++ isPrime.h
@@ -0,0 +1,18 @@
+/***
+* Author: Ian Reymond
+* Input: number to be checked for prime
+* Output: boolean value, true is prime, false is not prime
+***/
+
+bool isPrime(int n)
+{
+  int d = 0;
+  for(d=2; d<sqrt(n); ++d)
+  {
+    if(n%d == 0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
